fcf7737d55d5c5f62f388744e458bc9768bed3f8
Jude Ibe <judeibe@gmail.com>
2011-03-27 23:24:02 -0500

    deleted Me.java bad implementation in User.java now as overload

--- src/api/highrise/User.java
+++ src/api/highrise/User.java
@@ -11,10 +11,26 @@ public class User extends HighriseEntity{
 	private String emailAddress;
 	private Calendar createdAt;
 	private Calendar updatedAt;
+	private String apiToken;
+	private String dropboxEmail;
 	
 	public User(HRAuth auth) {
 		super(auth);
-		Element userElement = super.get("/user.xml");
+		Element userElement = super.get("/me.xml");
+		
+		this.id				= ElementValue.getIntValue(userElement, "id");
+		this.name			= ElementValue.getTextValue(userElement, "name");
+		this.emailAddress	= ElementValue.getTextValue(userElement, "email-address");
+		this.createdAt		= ElementValue.getDateTimeValue(userElement, "created-at");
+		this.updatedAt		= ElementValue.getDateTimeValue(userElement, "updated-at");
+		this.apiToken		= ElementValue.getTextValue(userElement, "token");
+		this.dropboxEmail	= ElementValue.getTextValue(userElement, "dropbox");
+
+	}
+	
+	public User(HRAuth auth, int userId) {
+		super(auth);
+		Element userElement = super.get("/users/"+userId+".xml");
 		
 		this.id				= ElementValue.getIntValue(userElement, "id");
 		this.name			= ElementValue.getTextValue(userElement, "name");
@@ -57,5 +73,19 @@ public class User extends HighriseEntity{
 	 */
 	public Calendar getUpdatedAt() {
 		return updatedAt;
+	}
+	
+	/**
+	 * @return the apiToken
+	 */
+	public String getApiToken() {
+		return apiToken;
+	}
+
+	/**
+	 * @return the dropboxEmail
+	 */
+	public String getDropboxEmail() {
+		return dropboxEmail;
 	}
 }
